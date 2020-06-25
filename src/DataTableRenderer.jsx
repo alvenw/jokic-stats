fd8be7f4426200144b9d1f5f48f82e19f7182084
alvenw <wang.x.alven@gmail.com>
2020-06-25 10:50:35 -0700

    added all season averages

---
+++ src/DataTableRenderer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DataTable from 'react-data-table-component';
+import 'react-dropdown/style.css';
+import columns from './columns';
+
+const DataTableRenderer = () => {
+	
+}
--- src/SeasonAverage.jsx
+++ src/SeasonAverage.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Dropdown from 'react-dropdown';
 import columns from './columns';
 import StatsView from './StatsView'
 
 function SeasonAverage() {
-  const averagesURL = 'https://www.balldontlie.io/api/v1/season_averages?player_ids[]=246';
+  const [season, setSeason] = useState(columns.options[0]);
+  const sets = (option) => {
+    setSeason(option.label);
+  }
+
+  const averagesURL = 'https://www.balldontlie.io/api/v1/season_averages?player_ids[]=246&season=' + season.slice(0,4);
 
   return ( 
     <React.Fragment>
@@ -12,8 +18,9 @@ function SeasonAverage() {
         <div className="Dropdown-title">
           Career Stats
         </div>
+        <Dropdown options={columns.options} onChange={sets} placeholder="2019-2020" value={season} className="Dropdown-item"/>
       </div>
-      <StatsView url={averagesURL} columnFormat={columns.average} formatData={formatAverages} title={"Season Averages"}/>
+      <StatsView url={averagesURL} columnFormat={columns.average} formatData={formatAverages} title={season + " Season Averages"}/>
     </React.Fragment>
   )
 }
